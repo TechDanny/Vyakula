cbafa0653f361ead88f13cb08434442f7a0718e0
root <root@DESKTOP-TKLJENL.localdomain>
2023-11-28 00:33:01 +0300

    update on the app file

--- app.py
+++ app.py
@@ -1,24 +1,101 @@
-from flask import Flask, render_template
-from flask import redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_required, LoginManager, logout_user, UserMixin, login_user
+
 
 app = Flask(__name__)
-@app.route('/')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+db = SQLAlchemy(app)
+
+#User model for the Database
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    second_name = db.Column(db.String(20), nullable=False)
+    phone_no = db.Column(db.Integer, nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    confirm_password = db.Column(db.String(60), nullable=False)
+
+@app.route('/home')
+@login_required
 def index():
-    #abort(500)
-    return render_template("index.html")
+    user = current_user
+    return render_template("index.html", user=user)
 
-@app.route('/about')
-def about_page():
-    return redirect(url_for('index') + '#about')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-@app.route('/menu')
-def menu_page():
-    return redirect(url_for('index') + '#menu')
+@app.route('/')
+def landPage():
+    return render_template("landing-page.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        phone_no = request.form['phone_no']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Check if the username already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'danger')
+            return redirect(url_for('register'))
+        
+        if password != confirm_password:
+            flash('Passwords do not match. Please enter matching passwords.', 'danger')
+            return redirect(url_for('register'))
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username,
+                        password=hashed_password,
+                        first_name=first_name,
+                        second_name=second_name,
+                        phone_no=phone_no,
+                        email=email,
+                        confirm_password=confirm_password,
+                        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+    return render_template('login.html', user=user)
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('landPage'))
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"), 500
