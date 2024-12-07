cdfd8d0d75d157c1180ea9f1cf2ac905616cc721
TechDanny <kokidanny15@gmail.com>
2024-12-07 15:50:26 +0300

    Reinitialized git repository

---
+++ README.md
@@ -0,0 +1,50 @@
+<h2> Vyakula (Food Delivery Website) </h2>
+<hr>
+<img src="./static/images/Screenshot1.png">
+<br>
+<h3> Description </h3>
+<hr>
+<b>The project is named Vyakula</b> - Vyakula is a Swahili word that can be translated to English as Cuisine. The idea was inspired from a customer centric approach to create an experience centered around their preferences. To provide rich tapestry of local cuisines, we aimed to celebrate diversity through our menu. <br>We understand the joy of savoring exquisite dishes without leaving the comfort of your home. Our platform is a haven for food enthusiasts, offering a seamless and delightful food delivery experience. We noticed a lack of online platforms specifically dedicated to African cuisine, this have since been a driving force to create something unique and valuable to cater for our peoples' needs.<br>
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Features](#features)
+4. [Contributing](#contributing)
+5. [Contributors](#contributors)
+6. [License](#license)
+
+## Installation
+1. Clone the repository
+2. Navigate the project directory
+3. Run `pip install -r requirements.txt` to install the dependencies
+
+## Usage
+1. Run `flask run` to start the application
+2. Open your browser and type `http://localhost:5000`
+
+## Features
+1. User can register
+    <img src="./static/images/Register1.webp">
+2. User can login
+    <img src="./static/images/Login.webp">
+3. Admins can add, update and delete menu
+    <img src="./static/images/Screenshot2.png">
+4. User can browse the menu
+    <img src="./static/images/Screenshot3.png">
+
+## Contributing
+We welcome contributions! Please follow these guidelines:
+1. Fork the repository
+2. Create a new branch
+3. Make your changes
+4. Submit a pull request
+
+## Contributors
+1. Brian Jalang'o      - brianhannington94@gmail.com
+2. Duncan Musau        - kokidanny15@gmail.com
+3. Syphrose Wabulwenyi - swabulwenyi@gmail.com
+
+## License
+This project is licensed under the [MIT License](LICENSE)
---
+++ app.py
@@ -0,0 +1,235 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, send_from_directory, current_app, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_required, LoginManager, logout_user, UserMixin, login_user
+import os
+from werkzeug.utils import secure_filename
+import base64
+from sqlalchemy.exc import IntegrityError
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()  # Load environment variables
+
+# Update to use MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define models
+class MenuItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    image = db.Column(db.LargeBinary, nullable=True)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    second_name = db.Column(db.String(20), nullable=False)
+    phone_no = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    confirm_password = db.Column(db.String(255), nullable=False)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def base64_encode(data):
+    return base64.b64encode(data).decode('utf-8')
+
+app.jinja_env.filters['base64_encode'] = base64_encode
+
+def save_image(file):
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        with open(filepath, "rb") as image_file:
+            return image_file.read()
+    return None
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/home')
+@login_required
+def index():
+    user = current_user
+    menu_items = MenuItem.query.all()
+
+    if 'image' in request.files:
+        menu_items.image = save_image(request.files['image'])
+
+    return render_template("index.html", user=user, menu_items=menu_items)
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    item_id = request.json.get('itemId')
+    item = MenuItem.query.get_or_404(item_id)
+
+    cart_items = session.get('cart_items', [])
+    cart_items.append(item.name)
+    session['cart_items'] = cart_items
+
+    total_quantity = len(cart_items)
+    return jsonify({'total_quantity': total_quantity})
+
+@app.route('/cart')
+def cart():
+    cart_items = session.get('cart_items', [])
+    menu_items = MenuItem.query.filter(MenuItem.name.in_(cart_items)).all()
+    
+    total_price = sum(item.price for item in menu_items)
+
+    return render_template('cart.html', menu_items=menu_items, total_price=total_price)
+
+@app.route('/checkout')
+def checkout():
+    cart_items = session.pop('cart_items', [])
+    return render_template('checkout.html', cart_items=cart_items)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def landPage():
+    return render_template("landing-page.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        phone_no = request.form['phone_no']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Check if the username already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'danger')
+            return redirect(url_for('register'))
+        
+        if password != confirm_password:
+            flash('Passwords do not match. Please enter matching passwords.', 'danger')
+            return redirect(url_for('register'))
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username,
+                        password=hashed_password,
+                        first_name=first_name,
+                        second_name=second_name,
+                        phone_no=phone_no,
+                        email=email,
+                        confirm_password=confirm_password,
+                        )
+        
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Your account has been created!', 'success')
+            return redirect(url_for('login'))
+        except IntegrityError as e:
+            db.session.rollback()
+            flash('Email address is already in use. Please try another one', 'danger')
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+    return render_template('login.html', user=user)
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('landPage'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.route('/admin')
+def admin():
+    menu_items = MenuItem.query.all()
+    return render_template('admin.html', menu_items=menu_items)
+
+@app.route('/admin/add', methods=['POST'])
+def add_menu_item():
+    name = request.form['name']
+    price = float(request.form['price'])
+    description = request.form['description']
+
+    new_item = MenuItem(name=name, price=price, description=description)
+
+    if 'image' in request.files:
+        new_item.image = save_image(request.files['image'])
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    return redirect(url_for('admin'))
+
+@app.route('/admin/delete/<int:item_id>')
+def delete_menu_item(item_id):
+    item_to_delete = MenuItem.query.get_or_404(item_id)
+    db.session.delete(item_to_delete)
+    db.session.commit()
+
+    return redirect(url_for('admin'))
+
+@app.route('/admin/update/<int:item_id>', methods=['GET', 'POST'])
+def update_menu_item(item_id):
+    item_to_update = MenuItem.query.get_or_404(item_id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        price = float(request.form['price'])
+        description = request.form['description']
+
+        item_to_update.name = name
+        item_to_update.price = price
+        item_to_update.description = description
+
+        if 'image' in request.files:
+            item_to_update.image = save_image(request.files['image'])
+
+        db.session.commit()
+
+        return redirect(url_for('admin'))
+
+    return render_template('update.html', item=item_to_update)
+
+if __name__ == '__main__':
+    app.run(debug=True)
